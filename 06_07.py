1ab61ec35f377e514fd16c16e3ad89b9645de59e
Mohammad Javidi <m.javidii@yahoo.com>
2020-10-22 17:26:18 +0330

    Loop - define fuction

---
+++ 06_07.py
@@ -0,0 +1,17 @@
+# flow controll (if - loop - ...)
+#for loop
+for i in 1, 2, 3, 'start':
+    print (i)
+
+#define a fuction
+def say_hi_to(n):
+    print ('Hi '+n)
+
+for name in 'ali', 'reza', 'mina', '58', '32':
+    say_hi_to(name)
+
+for number in range(1, 10):
+    print (number, '-->', number * number)
+
+for u in 'hi', 2, 5, '43', '3333':
+    print (u, u*2)
