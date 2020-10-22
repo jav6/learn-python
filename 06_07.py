dde6a9572919c54d97b7b684fc6825bc8d57ab30
Mohammad Javidi <m.javidii@yahoo.com>
2020-10-22 18:01:47 +0330

    Refactor Document

--- 06_07.py
+++ 06_07.py
@@ -1,17 +1,22 @@
 # flow controll (if - loop - ...)
+
 #for loop
 for i in 1, 2, 3, 'start':
     print (i)
 
 #define a fuction
 def say_hi_to(n):
-    print ('Hi '+n)
+    print ('Hi '+n) #do it - on any input
 
 for name in 'ali', 'reza', 'mina', '58', '32':
-    say_hi_to(name)
+    say_hi_to(name) #call function
 
 for number in range(1, 10):
     print (number, '-->', number * number)
 
 for u in 'hi', 2, 5, '43', '3333':
     print (u, u*2)
+
+# + in python -> apped - two thing -or- math ex. -> +*/-
+# = for a=1
+# == for (a=1) == 1
