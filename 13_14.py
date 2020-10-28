0db199461001bc7ba890f12c17ac65bb47d2d606
Mohammad Javidi <m.javidii@yahoo.com>
2020-10-28 22:01:08 +0330

    Loop -> while - list

---
+++ 13_14.py
@@ -0,0 +1,32 @@
+#for dbug use print
+#while -> loop
+#solve problem -> line to line
+
+
+print('hi')
+
+i=0
+while (i < 11):
+    print('num', i)
+    i += 1
+
+#a list
+a = []
+
+# for get type
+print(type(a))
+
+aa = [10, 20, 50, 45456, 654465]
+
+print(aa[0])
+
+#change value
+aa[0]=100
+
+print(aa[0])
+print(aa[0:2])
+print(aa[:2])
+print(aa[1:])
+
+# for get lenght
+len(aa)
