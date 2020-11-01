0311c14b0b9b2dca46f94587e7d86389e51bc2e8
Mohammad Javidi <m.javidii@yahoo.com>
2020-11-01 20:27:37 +0330

    Learn dictionary in python

---
+++ 17.py
@@ -0,0 +1,23 @@
+# use pylint in ide for check syntax
+
+tel={} #-> dic
+#tel[] -> list
+
+tel={'hadi':'+98-0054', 'saeed':'00254'}
+tel['mahdi1']='+1-0025'
+#tel={'mahdi2':'+1-0025'}
+
+print(tel)
+
+del tel['mahdi1']
+
+print(tel)
+
+print(tel['saeed'])
+
+#tel.get() for set empty
+#tel.key() for show key and value
+#x in tel if isset x -> true
+
+for a in tel.items():
+    print(a)
