fa207eb87241f6ff80ecf1ca5fc899c3cbc78b32
Mohammad Javidi <m.javidii@yahoo.com>
2021-01-07 20:46:32 +0330

    Add more detail about variable

--- 01_05.py
+++ 01_05.py
@@ -6,6 +6,16 @@ number  = 1
 number2 = 4
 number = 3
 str_str = "string"
+"""
+Variable data type :
+Text Type: 	str
+Numeric Types: 	int, float, complex
+Sequence Types: 	list, tuple, range
+Mapping Type: 	dict
+Set Types: 	set, frozenset
+Boolean Type: 	bool
+Binary Types: 	bytes, bytearray, memoryview
+"""
 
 #if statement
 if number > 0:
